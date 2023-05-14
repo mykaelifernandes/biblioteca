8e09e0b65be62ecd4539bee74f7225c61560e74b
Mykaeli <mykaelifernandes@hotmail.com>
2023-05-13 23:43:07 -0300

    GET game By id

--- src/main/java/com/projeto/biblioteca/controllers/LivroController.java
+++ src/main/java/com/projeto/biblioteca/controllers/LivroController.java
@@ -1,10 +1,12 @@
 package com.projeto.biblioteca.controllers;
 
+import com.projeto.biblioteca.dto.LivroDTO;
 import com.projeto.biblioteca.dto.LivroMinDTO;
 import com.projeto.biblioteca.entities.Livro;
 import com.projeto.biblioteca.services.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class LivroController {
     @Autowired
     private LivroService livroService;
 
+    @GetMapping(value = "/{id}")
+    public LivroDTO findById(@PathVariable Long id) {
+        LivroDTO result = livroService.findById(id);
+        return result;
+
+    }
     @GetMapping
     public List<LivroMinDTO> findAll() {
         List<LivroMinDTO> result = livroService.findAll();
---
+++ src/main/java/com/projeto/biblioteca/dto/LivroDTO.java
@@ -0,0 +1,77 @@
+package com.projeto.biblioteca.dto;
+
+import com.projeto.biblioteca.entities.Livro;
+import org.springframework.beans.BeanUtils;
+
+public class LivroDTO {
+
+    private Long id;
+    private String titulo;
+    private Integer ano;
+    private String autor;
+    private String genero;
+    private String imgUrl;
+    private String descricao;
+
+    public LivroDTO(){}
+
+    public LivroDTO(Livro entity){
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
